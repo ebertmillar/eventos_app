7a0bdc02d4ca3a386cdb06a81b258d1aa1b4e1e7
ebertmillar <ebertmillar@gmail.com>
2024-11-02 19:44:18 +0100

    configurada variable de entorno .env - Environment y main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:eventos_app/config/constants/environment.dart';
 import 'package:eventos_app/config/themes/app_theme.dart';
 import 'package:eventos_app/presentation/screens/register_user_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+
+  await Environment.initEnvironment();
+
   runApp(const MainApp());
 }
 
@@ -11,6 +15,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    print(Environment.apiUrl);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme().getTheme(),      
